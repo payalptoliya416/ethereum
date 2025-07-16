a7b63d88ed94785910229c47454f61f27b9806ab
Ashish Makwana <85098548+ashishmakwana96am@users.noreply.github.com>
2025-07-16 11:15:45 +0530

    code

--- assets/js/main.js
+++ assets/js/main.js
@@ -293,9 +293,23 @@ function checkPassword() {
 
 function unlockContent() {
   document.getElementById("passwordPrompt").style.display = "none";
-  document.getElementById("mainContent").style.display = "block";
+  document.getElementById("mainContent").style.visibility = "visible";
+  document.getElementById("mainContent").style.pointerEvents = "auto";
   document.body.style.overflow = "auto";
 }
+
+window.onload = function () {
+  const granted = localStorage.getItem("access_granted");
+
+  if (granted === "true") {
+    unlockContent();
+  } else {
+    document.getElementById("passwordPrompt").style.display = "flex";
+    document.getElementById("mainContent").style.visibility = "hidden";
+    document.getElementById("mainContent").style.pointerEvents = "none";
+    document.body.style.overflow = "hidden";
+  }
+};
 
 window.onload = function () {
   const granted = localStorage.getItem("access_granted");
